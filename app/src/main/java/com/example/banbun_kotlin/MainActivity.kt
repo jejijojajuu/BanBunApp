d873381e1e6312e3ac9204ef8b65b94697f39679
jejijojajuu <jjuliaadvin01@gmail.com>
2024-03-24 18:10:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/banbun_kotlin/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.banbun_kotlin
+
+import Classifier
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.banbun_kotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.resources -> replaceFragment(Resources())
+                R.id.classifier -> replaceFragment(Classifier())
+                R.id.guide -> replaceFragment(Guide())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+            }
+
+            true
+        }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        // Hide system bars
+        window.insetsController?.hide(WindowInsets.Type.systemBars())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+}
